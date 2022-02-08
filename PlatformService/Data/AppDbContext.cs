6208c338c55307ca1352ae44572d628219db2554
metehanyetkin12@gmail.com <metehanyetkin12@gmail.com>
2022-02-08 11:31:19 +0300

    Initial commit

---
+++ PlatformService/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Data
+{
+    //EF CORE DbContext
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option):base(option)
+        {
+            // Database Location Integration
+        }
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
---
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Profiles
+{
+    //Using AutoMapper
+    public class PlatformsProfile:Profile
+    {
+        public PlatformsProfile()
+        {
+            //Source --> Target 
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<PlatformReadDto, Platform>();
+            CreateMap<Platform, PlatformCreateDto>();
+            CreateMap<PlatformCreateDto, Platform>();
+        }
+
+    }
+}
